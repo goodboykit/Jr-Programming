3a53263eb60254f7594b41003a7227b5908f0292
Kit Nicholas Santiago <santiagogoodboykit@gmail.com>
2024-10-31 10:55:55 -0700

    commit new

--- Assets/Optimization/Scripts/OptimUnit.cs
+++ Assets/Optimization/Scripts/OptimUnit.cs
@@ -31,45 +31,61 @@ public class OptimUnit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Start profiling for "Handling Time"
+        Profiler.BeginSample("Handling Time");
         HandleTime();
+        Profiler.EndSample();
 
+        // Start profiling for "Rotating"
+        Profiler.BeginSample("Rotating");
         var t = transform;
 
-        if(transform.position.x <= 0)
+        if (transform.position.x <= 0)
             transform.Rotate(currentAngularVelocity * Time.deltaTime, 0, 0);
-        else if(transform.position.x > 0)
-            transform.Rotate(-currentAngularVelocity * Time.deltaTime, 0 ,0);
-        
-        if(transform.position.z >= 0)
-            transform.Rotate(0,0, currentAngularVelocity * Time.deltaTime);
-        else if(transform.position.z < 0)
-            transform.Rotate(0,0, -currentAngularVelocity * Time.deltaTime);
-        
-        Move();
+        else if (transform.position.x > 0)
+            transform.Rotate(-currentAngularVelocity * Time.deltaTime, 0, 0);
 
-        //check if we are moving away from the zone and invert velocity if this is the case
+        if (transform.position.z >= 0)
+            transform.Rotate(0, 0, currentAngularVelocity * Time.deltaTime);
+        else if (transform.position.z < 0)
+            transform.Rotate(0, 0, -currentAngularVelocity * Time.deltaTime);
+
+        Profiler.EndSample(); // end profiling for "Rotating"
+
+        // Start profiling for "Moving"
+        Profiler.BeginSample("Moving");
+        Move();
+        Profiler.EndSample(); // end profiling for "Moving"
+
+        // Start profiling for "Boundary Check"
+        Profiler.BeginSample("Boundary Check");
+
+        // Check if we are moving away from the zone and invert velocity if this is the case
         if (transform.position.x > areaSize.x && currentVelocity.x > 0)
         {
             currentVelocity.x *= -1;
-            PickNewVelocityChangeTime(); //we pick a new change time as we changed velocity
+            PickNewVelocityChangeTime(); // Pick a new change time as we changed velocity
         }
         else if (transform.position.x < -areaSize.x && currentVelocity.x < 0)
         {
             currentVelocity.x *= -1;
             PickNewVelocityChangeTime();
         }
-        
+
         if (transform.position.z > areaSize.z && currentVelocity.z > 0)
         {
             currentVelocity.z *= -1;
-            PickNewVelocityChangeTime(); //we pick a new change time as we changed velocity
+            PickNewVelocityChangeTime(); // Pick a new change time as we changed velocity
         }
         else if (transform.position.z < -areaSize.z && currentVelocity.z < 0)
         {
             currentVelocity.z *= -1;
             PickNewVelocityChangeTime();
         }
+
+        Profiler.EndSample(); // end profiling for "Boundary Check"
     }
+
 
 
     private void PickNewVelocity()
@@ -96,20 +112,9 @@ public class OptimUnit : MonoBehaviour
 
     void Move()
     {
-        Vector3 position = transform.position;
-        
-        float distanceToCenter = Vector3.Distance(Vector3.zero, position);
-        float speed = 0.5f + distanceToCenter / areaSize.magnitude;
-        
-        int steps = Random.Range(1000, 2000);
-        float increment = Time.deltaTime / steps;
-        for (int i = 0; i < steps; ++i)
-        {
-            position += currentVelocity * increment * speed;
-        }
-        
-        transform.position = position;
+        transform.position = transform.position + currentVelocity * Time.deltaTime;
     }
+
 
     private void HandleTime()
     {
@@ -127,4 +132,6 @@ public class OptimUnit : MonoBehaviour
             PickNewAngularVelocityChangeTime();
         }
     }
+
+
 }
